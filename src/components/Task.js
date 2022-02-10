ed1bf83a10b0542739458d3802bd9d24dc51708b
Luis Auz <luisauz.geek@gmail.com>
2022-02-09 23:42:36 -0500

    feat: adding states to control task finished

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,19 @@
+import { useState } from "react"
 import "../index.css"
 
 export const Task = ({nameTask}) => {
+
+    const [done, setDone] = useState(false)
+    
+    function handleClick() {
+        setDone(!done)
+    }
+    
+
     return(
         <div className="task">
             <span>{nameTask}</span>
-            <button className="button">✔</button>
+            <button onClick={handleClick} className={done ? "done" : "not-finished"}>{done ? "✔" : "❌"}</button>
         </div>
     )
 }
